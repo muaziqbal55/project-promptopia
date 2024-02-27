f5ff8cd02bb19b21f67bf4fa537c8c38aa00910c
Muaz Iqbal <muaz.iqbal@devsinc.com>
2024-02-27 11:02:19 -0800

    completed

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database.js'
+import User from "@models/user";
+import { Profiler } from "react";
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }
+        )
+    ],
+    callbacks: {
+        async session({ session }){
+            const userSession = await User.findOne({
+                email: session.user.email
+            })
+            session.user.id = userSession._id.toString()
+            return session
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB()
+                const userExists = await User.findOne({
+                    email: profile.email
+                })
+                if(!userExists){
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replaceAll(" ","").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true
+            }
+            catch(error){
+                console.log(error)
+                return false
+            }
+        }
+    }
+})
+
+export { handler as GET, handler as POST };
---
+++ components/Feed.jsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useState, useEffect } from 'react'
+import PromptCard from "@components/PromptCard"
+
+const PromptCardList = ({ data, handleTagClick })=>{
+
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post)=>{
+        return(
+          <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+const Feed = () => {
+  const [searchText,serSearchText] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = (e)=>{
+
+  }
+
+  useEffect(()=>{
+    const fetchPosts = async ()=>{
+      const response = await fetch('/api/prompt')
+      const data = await response.json();
+      setPosts(data)
+    }
+    fetchPosts()
+  },[])
+
+  return (
+   <section className='feed'>
+    <form className='relative w-full flex-center'>
+      <input type="text"
+        placeholder='Search For A Tag'
+        value={searchText}
+        onChange={handleSearchChange}
+        required
+        className='search_input peer'
+      />
+    </form>
+
+    <PromptCardList
+      data={posts}
+      handleTagClick={()=>{}}
+    />
+   </section>
+  )
+}
+
+export default Feed
